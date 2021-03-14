36efaada9075a040b7bba3bfcdd3d3a142e17d3b
Oleksandr Zakharchuk <zahirolek@gmail.com>
2021-03-14 18:08:37 +0200

    commit -Technical Interview-Example

---
+++ src/ua/TechnicalInterveByJava/Examples/DExample.java
@@ -0,0 +1,29 @@
+package ua.TechnicalInterveByJava.Examples;
+
+public class DExample {
+    public static void main(String[] args) {
+        // int sum=0;
+        int a = 3;
+        int b = 100;
+        a+=6561;
+        while (a < b) ;
+
+        a = a * b;
+
+
+
+        //  for ( ; a < b; a +=1,a=a*b   ) ;
+
+        System.out.println("a равен " + a + " ; b равен " + b);
+
+      /*  int a=3;
+        int b=100;
+        while (a<b) {
+            a=a*b;*/
+    }
+
+    // System.out.println(a);
+
+
+}
+
---
+++ src/ua/TechnicalInterveByJava/Examples/EExample.java
@@ -0,0 +1,15 @@
+package ua.TechnicalInterveByJava.Examples;
+
+/*
+public class EExample {
+    public static void main(String[] args) {
+*/
+
+  /*  }
+    ArrayA={1,2,3,4,5,}
+    ArrayB={5,6,7,8,9}
+    For Num 1 to 3
+    sout  ArrayB[ArrayA[Num]]
+    End for
+*/
+
