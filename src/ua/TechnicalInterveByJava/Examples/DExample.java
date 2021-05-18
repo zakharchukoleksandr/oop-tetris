ab8367d315a58329e721fd293d87eca534aff050
Oleksandr Zakharchuk <zahirolek@gmail.com>
2021-05-18 10:56:36 +0300

    commit -Technical Interview-Example rename AgeDifferences

--- src/ua/TechnicalInterveByJava/Examples/DExample.java
+++ src/ua/TechnicalInterveByJava/Examples/DExample.java
@@ -1,57 +1,24 @@
 package ua.TechnicalInterveByJava.Examples;
 
-public class AgeDifferents {
+public class DExample {
     public static void main(String[] args) {
 
-        int age;
-        int age1man =40;
-        int age2man=20;
 
-        if (age1man > age2man) {
-            int a = age1man - age2man;
-           System.out.println("если возраст 1-го  человека больше  возраста 2-го  человека  тогда 1-й человек старше другого на "  + a + " лет");
+        // int sum=0;
+        int a = 3;
+        int b = 100;
+        a += 6561;
+        while (a < b) ;
+        a = a * b;
 
-        }
-        else {
-            int b = age2man - age1man;
-            System.out.println(" \"если возраст 1-го  человека   меньше возраста 2-го  человека \"тогда 1-й человек моложе другого на " + b + " лет");
-        }
+
+        for (; a < b; a += 1, a = a * b) ;
+
+        System.out.println("a равен " + a + " ; b равен " + b);
     }
 }
 
-        // int sum=0;
-//        int a = 3;
-//        int b = 100;
-//        a+=6561;
-//        while (a < b) ;
-//
-//        a = a * b;
 
-
-        //  for ( ; a < b; a +=1,a=a*b   ) ;
-
-        //ystem.out.println("a равен " + a + " ; b равен " + b);
-
-//        int a = 30;
-//        int b = 10;
-//        System.out.println("a - возраст 1-го  человека" + a + "b - возраст 2-го человека" + b);
-//
-//        if (a > b) ;
-//        {
-//            int a = 30;
-//            int b = 10;
-//            System.out.println("если возраст 1-го  человека больше  возраста 2-го  человека ");
-//            int age = a - b;
-//            System.out.println(" тогда 1-й человек старше другого на " + age + " лет");
-//        }
-//            else  (a < b) ;
-//        int a = 10;
-//        int b = 30;
-//        System.out.println("если возраст 1-го  человека меньше  возраста 2-го  человека ");
-//        int age = b - a;
-//        System.out.println(" тогда 1-й человек моложе другого на " + age + " лет");
-   // }
-//}
 
 
 
